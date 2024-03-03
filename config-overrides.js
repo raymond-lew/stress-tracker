7f147203ff3be96cf3a0552eb86c693d2da90dd0
Raymond Lew <raymondlewly@gmail.com>
2024-03-03 17:52:42 +0800

    Tidy up the file

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+
 module.exports = function override(config, env) {
   config.resolve.fallback = {
     url: require.resolve("url"),
@@ -20,16 +21,4 @@ module.exports = function override(config, env) {
   return config;
 };
 
-{
-  /*module.exports = function override(config, env) {
-    // New config, e.g. config.plugins.push...
-    // console.log(JSON.stringify(config.resolve.fallback))
-     config.resolve.fallback = {
-         crypto: false,
-         util: false,
-         stream: false,
-         ...config.resolve.fallback
-     };  
-     return config
- }*/
-}
+
