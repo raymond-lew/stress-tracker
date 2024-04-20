84513eac45b461b6384eafd107ffbeba2a976413
Raymond Lew <raymondlewly@gmail.com>
2024-04-20 09:12:34 +0800

    Update

--- server/index.js
+++ server/index.js
@@ -20,6 +20,14 @@ app.use(cors());
 app.use("/api", Route);
 
 const path = require("path");
+
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
+{/*}
 app.use(express.static('../build'));
 app.get('*', (req, res)=> {
   const index = path.join(__dirname, '/', '../build', 'index.html' );
